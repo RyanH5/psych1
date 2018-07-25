f7614f0bc9ac18f381b4ac50b2f0d9a68a644703
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-07-25 13:29:22 -0600

    Modes css improvements

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -4,18 +4,18 @@ import "./Article.css";
 const Article = (props) => {
 
   return (
-    <div>
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
-      <h5>{props.author}</h5>
-      <h6>{props.source.name}</h6>    
+    <div className="single-article">
       <a 
         className="article-link" 
         target="_blank" 
         href={props.url}
         >
-        {props.url}
+        {props.title}
       </a> 
+      <p>{props.description}</p>
+      <h5>{props.author}</h5>
+      <h6>{props.source.name}</h6>    
+      
     </div>
   )
 }
